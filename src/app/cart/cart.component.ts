6a394fea690a31b943e64f5817de35bc52ae0918
parthpanchal5 <panchalparth658@gmail.com>
2022-01-25 21:29:43 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -7,9 +9,22 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
   items = this.cartService.getItems();
 
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.log('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
   removeItem() {
     this.items = this.cartService.clearCart();
   }
